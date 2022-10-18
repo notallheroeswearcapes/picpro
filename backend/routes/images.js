2f40ca77377fafd71c1c104b57f7e0f132b791f2
matthias.eder <eder.matthias1@gmail.com>
2022-10-18 15:03:49 +1000

    added sharp to backend; created stub for image transformation endpoint

--- backend/routes/images.js
+++ backend/routes/images.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const express = require('express');
+const sharp = require("sharp");
 require('dotenv').config();
 
 const router = express.Router();
@@ -7,6 +8,10 @@ const router = express.Router();
 router.post('/upload', (req, res) => {
     console.log("⚡️ Received request to /upload");
     res.send("Received image successfully.")
+});
+
+router.post('/transform', (req, res) => {
+    // main endpoint for image transformation, use sharp here
 });
 
 module.exports = router;
