bcc9aed488bc13f9bd91b393a520123e1479daec
EnZanity <49771675+EnZanity@users.noreply.github.com>
2022-10-23 21:47:10 +1000

    placeholder redis

--- backend/routes/presets.js
+++ backend/routes/presets.js
@@ -1,58 +1,49 @@
 const axios = require('axios');
 const express = require('express');
 require('dotenv').config();
-const redis = require('redis');
-const redisClient = redis.createClient();
 
 const router = express.Router();
 
-redisKeys = "dummy";
 // example of json response
 dummy = {
-    presets: {
+    presetName: "dummy",
+    transformation: {
+        outputType: "jpeg",
         width: 400,
         height: 200,
-        greyscale: true,
-        blackwhite: false,
-        brightness: false,
-        bri_set: 2,
-        saturation: false,
-        sat_set: 0.2,
-        hue: false,
-        hue_set: 180,
+        flip: false,
+        flop: false,
+        sharpen: false,
         blur: false,
-        file: "jpeg"
+        greyscale: true,
+        blackwhite: false
     }
 }
 
+// example of json response
+dummy1 = {
+    presetName: "dummy1",
+    transformation: {
+        outputType: "png",
+        width: 200,
+        height: 400,
+        flip: false,
+        flop: false,
+        sharpen: false,
+        blur: true,
+        greyscale: false,
+        blackwhite: false
+    }
+}
+
+// This section will change for Cloud Services
+// Redis setup
+const redis = require('redis');
+const redisClient = redis.createClient();
+redisClient.connect()
+
 router.get('/test', (req, res) => {
     console.log("⚡️ Received request to /presets/test");
-
-    redisClient.connect()
-        .catch((err) => {
-            console.log(err);
-        });
-
-    // save in redis
-    // const redisJSON = JSON.parse(dummy.presets);
-    const redisJSON = dummy.presets;
-    redisJSON.source = "Redis Cache";
-    redisClient.setEx(
-        redisKeys,
-        3600,
-        JSON.stringify({ source: "Redis Cache", ...redisJSON })
-    );
-
-    // // retreive from redis
-    // redisClient.get(redisKeys).then((result) => {
-    //     if (result) {
-    //         // insert code
-    //         console.log(result)
-    //     }
-    // })
-    // .catch((err) => {
-    //     console.error(`❌ Error when fetching preset \'${redisKeys}\' from redis: ${err}`);
-    // });
 
     res.send("Received successfully.")
 });
@@ -60,13 +51,35 @@ router.get('/test', (req, res) => {
 router.get('/', (req, res) => {
     console.log("⚡️ Received request to /presets/");
 
-    // return the "name" attributes of all stored preset objects in redis 
+    // return the "name" attributes of all stored preset objects in redis
+    // redisClient.get(dummy.presetName).then((result) => {
+    //     if (result) {
+    //         // insert code
+    //         console.log(result);
+    //         res.send(result);
+    //     }
+    // })
+    // .catch((err) => {
+    //     console.error(`❌ Error when fetching preset * from redis: ${err}`);
+    // });
 })
 
 router.post('/fetch', (req, res) => {
     console.log("⚡️ Received request to /presets/fetch");
 
     // return a specific preset object by its "name" attribute that is passed in the request body
+    // retreive from redis
+    const redisKey = dummy.presetName
+    redisClient.get(redisKey).then((result) => {
+        if (result) {
+            // insert code
+            console.log(result);
+            res.send(result);
+        }
+    })
+    .catch((err) => {
+        console.error(`❌ Error when fetching preset \'${redisKey}\' from redis: ${err}`);
+    });
 })
 
 router.post('/upload', (req, res) => {
@@ -74,6 +87,20 @@ router.post('/upload', (req, res) => {
 
     // upload a a preset object that is passed in the request body to redis and return a boolean if the operation was successful
     // we might change this later to a PUT method depending if we really need the success boolean, but it's fine for now
+    const redisJSON = dummy.transformation;
+    const redisKey = dummy.presetName;
+    redisClient.setEx(
+        redisKey,
+        3600,
+        JSON.stringify({ redisJSON })
+    ).then((result) => {
+        console.log(`✅ Successfully uploaded preset \'${redisKey}\' to Redis: ${result}`);
+        res.send(`✅ Successfully uploaded preset \'${redisKey}\' to Redis: ${result}`);
+    })
+    .catch((err) => {
+        console.error(`❌ Error during upload of preset \'${redisKey}\' to Redis: ${err}`);
+        res.send(`❌ Error during upload of preset \'${redisKey}\' to Redis: ${err}`);
+    });
 })
 
 module.exports = router;
